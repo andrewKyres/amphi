b38fe28e7831bfeaa2737ef90a2bc0a981ae1e77
Andrew <andrewkyres117@gmail.com>
2018-08-10 21:30:33 -0400

    Add debug option for VM that runs a single iteration

--- src/vm.rs
+++ src/vm.rs
@@ -7,7 +7,9 @@ pub struct VM {
     /// Program counter that tracks which byte is being executed
     pc: usize,
     /// The bytecode of the program being run
-    program: Vec<u8>
+    program: Vec<u8>,
+    /// Whether to run the VM in debug mode
+    debug: bool
 }
 
 impl VM {
@@ -17,31 +19,37 @@ impl VM {
             registers: [0; 32],
             program: vec![],
             pc: 0,
+            debug: false,
         }
     }
 
     /// Starts the primary execution loop
-    pub fn run(&mut self) {
+    pub fn run(&mut self) -> u8 {
         loop {
-            // If our program coutner has exceeded the length of the program itself, something has
-            // gone awry
+            /// Our program counter can't exceed the length of the program
             if self.pc >= self.program.len() {
-                break;
+                println!("pc exceeds program length");
+                return 1;
             }
             match self.decode_opcode() {
                 Opcode::HLT => {
                     println!("HLT encountered");
-                    return;
+                    return 0;
                 },
                 _ => {
                     println!("Unrecognized opcode found! Terminating!");
-                    return;
+                    return 1;
                 }
+            }
+
+            if self.debug {
+                println!("debug iteration complete");
+                return 0;
             }
         }
     }
 
-    /// Attempts to decode the byte the VM's program coutner is pointing at into an opcode
+    /// Decodes the byte in which the VM's program counter is pointing to, into an opcode
     fn decode_opcode(&mut self) -> Opcode {
         let opcode = Opcode::from(self.program[self.pc]);
         self.pc += 1;
@@ -75,6 +83,12 @@ mod tests {
         test_vm.program = test_bytes;
         test_vm.run();
         assert_eq!(test_vm.pc, 1);
+    }
+
+    #[test]
+    fn test_debug_mode() {
+        let mut test_vm = VM::new();
+        assert_eq!(test_vm.run(), 0);
     }
 }
 
